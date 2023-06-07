2996821c3e8e4c8d14827b50f0bf155770a2189d
EvgeniyKauter <evgeniykauter@mail.ru>
2023-06-07 21:39:50 +0300

    update b-g.js, package.json; add cli.js, package-lock.json

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import readlineSync from "readline-sync";
+import {readlineSync} from "../src/cli.js";
 
 console.log("node bin/brain-games.js\nWelcome to the Brain Games!");
-const name = readlineSync.question('What is your name?');
+const name = readlineSync.question('What is your name? ');
 console.log(`Hello, ${name}!`);
