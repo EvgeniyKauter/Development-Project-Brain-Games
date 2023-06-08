a4f7890acc17bb5cc07a77d07e46056c38e48709
EvgeniyKauter <evgeniykauter@mail.ru>
2023-06-08 21:44:39 +0300

    update makefile, package.json and add brain-even

---
+++ bin/brain-even.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+import readlineSync from '../src/cli.js';
+
+console.log('brain-even\n\nWelcome to the Brain Games!');
+const name = readlineSync.question('What is your name? ');
+console.log(`Hello, ${name}!`);
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+let checker = 0;
+let number = 0;
+let answer = '';
+while (checker < 3) {
+  number = Math.floor(Math.random() * 100);
+  console.log(`Question: ${number}`);
+  answer = readlineSync.question('Your answer: ');
+  if (number % 2 === 0) {
+    if (answer === 'yes') {
+      console.log('Correct!');
+      checker += 1;
+    } else {
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "yes".`);
+      break;
+    }
+  }
+  if (number % 2 !== 0) {
+    if (answer === 'no') {
+      console.log('Correct!');
+      checker += 1;
+    } else {
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "no".`);
+      break;
+    }
+  }
+}
+if (checker === 3) {
+  console.log(`Congratulations, ${name}`);
+}
