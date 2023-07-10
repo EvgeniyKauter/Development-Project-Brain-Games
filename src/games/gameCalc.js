a740062aa6c30f23e8680b5af4e2ab19450b4b76
EvgeniyKauter <evgeniykauter@mail.ru>
2023-07-10 23:31:46 +0300

    update scr/

--- src/games/gameCalc.js
+++ src/games/gameCalc.js
@@ -3,23 +3,23 @@ import getRandomNumber from '../helper.js';
 
 const rule = 'What is the result of the expression?';
 
-const calculate = (number1, number2, randomChar) => {
-  switch (randomChar) {
+const calculate = (number1, number2, randomOperator) => {
+  switch (randomOperator) {
     case '+': return number1 + number2;
     case '-': return number1 - number2;
     case '*': return number1 * number2;
-    default: throw new Error(`Unknown item ${randomChar}`);
+    default: throw new Error(`Unknown item ${randomOperator}`);
   }
 };
 
 const makelogic = () => {
-  const char = ['*', '-', '+'];
+  const operators = ['*', '-', '+'];
   const number1 = getRandomNumber(1, 100);
   const number2 = getRandomNumber(1, 100);
   const index = getRandomNumber(0, 2);
-  const rezult = calculate(number1, number2, char[index]);
+  const rezult = calculate(number1, number2, operators[index]);
   const expectedAnswer = String(rezult);
-  const question = `${number1} ${char[index]} ${number2}`;
+  const question = `${number1} ${operators[index]} ${number2}`;
   return [expectedAnswer, question];
 };
 
