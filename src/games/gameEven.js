df9de72a1c97e41ddc2f887cbf9fd43e5a66cfac
EvgeniyKauter <evgeniykauter@mail.ru>
2023-07-10 23:18:37 +0300

    update gameEven.js

--- src/games/gameEven.js
+++ src/games/gameEven.js
@@ -6,9 +6,8 @@ const rule = 'Answer "yes" if the number is even, otherwise answer "no".';
 const isEven = (number) => number % 2 === 0;
 
 const makelogic = () => {
-  const number = getRamdomNumber(1, 100);
-  const question = number;
-  const expectedAnswer = isEven(number) ? 'yes' : 'no';
+  const question = getRamdomNumber(1, 100);
+  const expectedAnswer = isEven(question) ? 'yes' : 'no';
   return [expectedAnswer, question];
 };
 
